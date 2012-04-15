077964ceaa2ee94e9dc369e7c3cca9251c7578f6
stereobooster <slavic.v@gmail.com>
2012-04-16 01:12:19 +0300

    add missing `var` keywords

--- static/hn.js
+++ static/hn.js
@@ -1,14 +1,16 @@
-(function(d){
-	var hnAnchorElements = document.getElementsByClassName("hn-share-button");
-	for(var e = hnAnchorElements.length - 1; e >= 0; e--) {
-		title = hnAnchorElements[e].getAttribute("data-title") || window.document.title;
-		url   = hnAnchorElements[e].getAttribute("data-url")   || window.location.href;
-
-		var i = document.createElement("iframe");
+(function(window){
+	var document = window.document,
+		hnAnchorElements = document.getElementsByClassName("hn-share-button"),
+		e;
+	for (e = hnAnchorElements.length - 1; e >= 0; e--) {
+		var anchor = hnAnchorElements[e],
+			title = anchor.getAttribute("data-title") || document.title,
+			url = anchor.getAttribute("data-url")   || window.location.href,
+			i = document.createElement("iframe");
 		i.src = "http://hnbutton.appspot.com/button?title="+encodeURIComponent(title)+"&url="+encodeURIComponent(url);
 		i.scrolling = "auto"; i.frameBorder = "0"; i.width = "75px"; i.height = "20px";
 
-		hnAnchorElements[e].parentNode.insertBefore(i, hnAnchorElements[e]);
-		hnAnchorElements[e].parentNode.removeChild(hnAnchorElements[e]);
+		anchor.parentNode.insertBefore(i, anchor);
+		anchor.parentNode.removeChild(anchor);
 	}
-})(document);
+})(window);
