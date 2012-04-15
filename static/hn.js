fd9e14180928a0cd3cc9d529747df38d2e67b10e
Ilya Grigorik <ilya@igvita.com>
2012-04-15 15:11:05 -0700

    auto-instrument button with trackSocial (google analytics)

--- static/hn.js
+++ static/hn.js
@@ -1,4 +1,14 @@
 (function(d){
+	var eventMethod = window.addEventListener ? "addEventListener" : "attachEvent";
+	var eventer = window[eventMethod];
+	var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+
+	eventer(messageEvent,function(e) {
+		if (typeof(_gaq) != "undefined") {
+			_gaq.push(['_trackSocial', 'hacker news', e.data])
+		}
+	},false);
+
 	var hnAnchorElements = document.getElementsByClassName("hn-share-button");
 	for(var e = hnAnchorElements.length - 1; e >= 0; e--) {
 		title = hnAnchorElements[e].getAttribute("data-title") || window.document.title;
