b4ddad6d3322d247adf1266c77b508537f22542d
slavic <stereobooster@gmail.com>
2012-04-16 08:58:25 +0300

    chage window to w GA now truely asynchronous follow jshint rules add min version

--- static/hn.js
+++ static/hn.js
@@ -1,28 +1,32 @@
-(function(w){
-  var eventMethod = w.addEventListener ? "addEventListener" : "attachEvent";
-  var eventer = w[eventMethod];
-  var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
+/*jshint forin:true, noarg:true, noempty:true, eqeqeq:true, bitwise:true, strict:true, undef:true, curly:true, browser:true, indent:2, maxerr:50, expr:true */
+(function (w) {
+  "use strict";
+  var j,
+    d = w.document,
+    hnAnchorElements = d.getElementsByClassName("hn-share-button"),
+    eventMethod = w.addEventListener ? "addEventListener" : "attachEvent",
+    eventer = w[eventMethod],
+    messageEvent = eventMethod === "attachEvent" ? "onmessage" : "message",
+    base = "http://hnbutton.appspot.com/";
 
-  eventer(messageEvent,function(e) {
-    if (e.origin !== 'http://hnbutton.appspot.com/') { return; }
-    if (typeof(_gaq) != "undefined") {
-      if (e.data == 'vote' || e.data == 'submit') {
-        _gaq.push(['_trackSocial', 'Hacker News', e.data]);
-      }
+  w._gaq || (w._gaq = []);
+  eventer(messageEvent, function (e) {
+    if (e.origin === base && (e.data === "vote" || e.data === "submit")) {
+      w._gaq.push(["_trackSocial", "Hacker News", e.data]);
     }
   }, false);
 
-  var d = window.document, e,
-      hnAnchorElements = d.getElementsByClassName("hn-share-button");
+  for (j = hnAnchorElements.length - 1; j >= 0; j--) {
+    var anchor = hnAnchorElements[j],
+      title = anchor.getAttribute("data-title") || d.title,
+      url = anchor.getAttribute("data-url") || w.location.href,
+      i = d.createElement("iframe");
 
-  for(e = hnAnchorElements.length - 1; e >= 0; e--) {
-    var anchor = hnAnchorElements[e],
-        title = anchor.getAttribute("data-title") || d.title,
-        url = anchor.getAttribute("data-url") || w.location.href,
-        i = d.createElement("iframe");
-
-    i.src = "http://hnbutton.appspot.com/button?title="+encodeURIComponent(title)+"&url="+encodeURIComponent(url);
-    i.scrolling = "auto"; i.frameBorder = "0"; i.width = "75px"; i.height = "20px";
+    i.src = base + "button?title=" + encodeURIComponent(title) + "&url=" + encodeURIComponent(url);
+    i.scrolling = "auto"; 
+    i.frameBorder = "0"; 
+    i.width = "75px"; 
+    i.height = "20px";
 
     anchor.parentNode.insertBefore(i, anchor);
     anchor.parentNode.removeChild(anchor);
