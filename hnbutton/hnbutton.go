cf17ccbdcc0f57cd30c7ce90b1c51552c5183f57
Ilya Grigorik <ilya@igvita.com>
2012-04-23 12:15:31 -0500

    add public header for edge cache

--- hnbutton/hnbutton.go
+++ hnbutton/hnbutton.go
@@ -81,7 +81,8 @@ func Button(w http.ResponseWriter, r *http.Request) {
 
     // Cache the response in the HTTP edge cache, if possible
     // http://code.google.com/p/googleappengine/issues/detail?id=2258
-    w.Header().Set("Cache-Control", "public, max-age=61;")
+    w.Header().Set("Cache-Control", "public, max-age=61")
+    w.Header().Set("Pragma", "Public")
 
     if hnreply.Hits == 0 {
         c.Infof("No hits, rendering submit template")
